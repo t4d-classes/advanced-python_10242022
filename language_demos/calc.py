5440fef3fe4e622868b86fb131ad03a46ad7b654
Eric Greene <eric@t4d.io>
2022-10-24 14:49:42 -0400

    add solution to exercise 1

---
+++ language_demos/calc.py
@@ -0,0 +1,89 @@
+
+result = 0.0
+history = []
+
+while True:
+
+    command = input("Enter a command:")
+
+    if command == "add":
+        operand = float(input("Please enter an operand:"))
+        result = result + operand
+
+        next_id = 1
+        if history:
+            next_id = max([ entry["id"] for entry in history ]) + 1
+
+        history.append({
+            "id": next_id,
+            "opName": "add",
+            "opValue": operand
+        })
+
+    elif command == "subtract":
+        operand = float(input("Please enter an operand:"))
+        result = result - operand
+
+        next_id = 1
+        if history:
+            next_id = max([ entry["id"] for entry in history ]) + 1
+
+        history.append({
+            "id": next_id,
+            "opName": "add",
+            "opValue": operand
+        })
+
+    elif command == "multiply":
+        operand = float(input("Please enter an operand:"))
+        result = result * operand
+
+        next_id = 1
+        if history:
+            next_id = max([ entry["id"] for entry in history ]) + 1
+
+        history.append({
+            "id": next_id,
+            "opName": "add",
+            "opValue": operand
+        })
+
+    elif command == "divide":
+        operand = float(input("Please enter an operand:"))
+        result = result / operand
+
+        next_id = 1
+        if history:
+            next_id = max([ entry["id"] for entry in history ]) + 1
+
+        history.append({
+            "id": next_id,
+            "opName": "add",
+            "opValue": operand
+        })
+
+    elif command == "exponent":
+        operand = float(input("Please enter an operand:"))
+        result = result ** operand
+
+        next_id = 1
+        if history:
+            next_id = max([ entry["id"] for entry in history ]) + 1
+
+        history.append({
+            "id": next_id,
+            "opName": "add",
+            "opValue": operand
+        })
+
+    elif command == "clear":
+        result = 0
+    elif command == "exit":
+        break
+    else:
+        print("Invalid Command, Try Again.")
+        continue
+
+    print("Result: ", result)
+    print("History: ", history)
+
