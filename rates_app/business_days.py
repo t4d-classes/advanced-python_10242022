cfea418fa1bee99656d63dc5608f95290a5409f1
Eric Greene <eric@t4d.io>
2022-10-26 15:41:01 -0400

    add solution to exercise 5

---
+++ rates_app/business_days.py
@@ -0,0 +1,16 @@
+from datetime import date, timedelta
+import holidays
+
+def business_days(start_date, end_date):
+    """ business days """
+
+    us_holidays = holidays.UnitedStates()
+
+    for day in range((end_date - start_date).days + 1):
+        the_date = start_date + timedelta(day)
+        if (the_date.weekday() < 5) and (the_date not in us_holidays):
+            yield the_date
+
+
+for business_day in business_days(date(2022, 6, 3), date(2022, 7, 5)):
+    print(business_day)
