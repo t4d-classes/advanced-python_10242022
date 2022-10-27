16543f86ea7c5d281b9be9e1d2183f4a9c51aab3
Eric Greene <eric@t4d.io>
2022-10-27 15:48:21 -0400

    add rates demo

--- rates_app/rates_demo.py
+++ rates_app/rates_demo.py
@@ -4,6 +4,7 @@ from datetime import date
 from concurrent.futures import ThreadPoolExecutor
 from business_days import business_days
 
+import threading
 import time
 import aiohttp
 import asyncio
@@ -32,34 +33,80 @@ def get_rates_sync(start_date, end_date):
         rate_responses.append(response.text)
 
     return rate_responses
-    
+
+
+def get_rate_thread(business_day, rate_responses):
+    response = requests.get(get_rate_url(business_day), timeout=60)
+    rate_responses.append(response.text)
+
 
 def get_rates_thread(start_date, end_date):
-    ...
+    rate_responses = []
+    threads: list[threading.Thread] = []
 
-def get_rates_async(start_date, end_date):
-    ...
+    for business_day in business_days(start_date, end_date):
+        a_thread = threading.Thread(target=get_rate_thread, args=(business_day, rate_responses))
+        a_thread.start()
+        threads.append(a_thread)
+
+    for a_thread in threads:
+        a_thread.join()
+    return rate_responses
+
+
+async def get_rate_async(session, business_day):
+    async with session.get(get_rate_url(business_day)) as resp:
+        return str(await resp.json())
+
+
+async def get_rates_async(start_date, end_date):
+    async with aiohttp.ClientSession() as session:
+        return await asyncio.gather(*[
+            get_rate_async(session, business_day)
+            for business_day in business_days(start_date, end_date)
+        ])
+
+
+def get_rate_threadpool(business_day):
+    response = requests.get(get_rate_url(business_day), timeout=60)
+    return response.text
+
 
 def get_rates_threadpool(start_date, end_date):
-    ...
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        return list(executor.map(
+            get_rate_threadpool,
+            list(business_days(start_date, end_date))
+        ))
 
 
 get_rates_funcs = [
-    ("Sync", get_rates_sync),
-    # ("Thread", get_rates_thread),
-    # ("Async", get_rates_async),
-    # ("ThreadPool", get_rates_threadpool),
+    ("Sync", get_rates_sync, False),
+    ("Thread", get_rates_thread, False),
+    ("Async", get_rates_async, True),
+    ("ThreadPool", get_rates_threadpool, False),
 ]
 
-for (func_name, get_rates) in get_rates_funcs:
+async def main():
 
-    start_time = time.time()
-    print(f"Running {func_name}")
+    for (func_name, get_rates, is_async) in get_rates_funcs:
 
-    rates = get_rates(start_date, end_date)
+        start_time = time.time()
+        print(f"Running {func_name}")
 
-    for rate in rates:
-        print(rate)
-    end_time = time.time()
+        rates = []
 
-    print(f"{func_name} time elapsed: {end_time - start_time}")
+        if is_async:
+            rates = await get_rates(start_date, end_date)
+        else:
+            rates = get_rates(start_date, end_date)
+
+        # for rate in rates:
+        #     print(rate)
+        end_time = time.time()
+
+        print(f"{func_name} time elapsed: {end_time - start_time}")
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
